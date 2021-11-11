0a85c50c43c9ebc2abab7aa6992965ce11a43a84
medlicence <93639705+medlicence@users.noreply.github.com>
2021-11-11 13:11:25 +0300

    Update docs.js

--- pages/docs.js
+++ pages/docs.js
@@ -3,19 +3,6 @@ export default function Docs() {
   return (
     <>
       <h1>Our docs !!!!</h1>
-
-      <div class = "links-in-div">
-  
-        <a href='https://egisz.rosminzdrav.ru/'>Единая государственная информационная система в сфере здравоохранения</a>
-        <a href="https://portal.egisz.rosminzdrav.ru/materials">ЕГИСЗ Портал взаимодействия</a>
-        <a href="https://support.egisz.rosminzdrav.ru/index.php?/Knowledge/List/Index/30">Центр поддержки Минздрава</a>
-        <a href="https://roszdravnadzor.gov.ru/opendata">Реестр наборов открытых данных</a>
-        <a href="https://xn--80ajghhoc2aj1c8b.xn--p1ai/upload/API_%D0%9C%D0%94%D0%9B%D0%9F_ru.pdf">Описание API Государственной информационной системы мониторинга и движения лекарственных препаратов</a>
-        <a href="https://partners.gosuslugi.ru">Сервисы электронного правительства</a>
-        <a href="https://partners.gosuslugi.ru/catalog/integration_module">Госуслуги интеграционный модуль</a>
-        <a href="https://data.gov.ru">Портал открытых данных Российской Федерации</a>
-  
-      </div>
     </>
   )
 }
