e5a4c52ddcf960ea9abf8bde68b3168db0ce4b5a
medlicence <93639705+medlicence@users.noreply.github.com>
2021-11-11 11:57:00 +0300

    Update docs.js

--- pages/docs.js
+++ pages/docs.js
@@ -2,6 +2,7 @@ export default function Docs() {
   return (
     <>
       <h1>Our docs !!!!</h1>
+      <a href='https://egisz.rosminzdrav.ru/'>Единая государственная информационная система в сфере здравоохранения</a>
     </>
   )
 }
